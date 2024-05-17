a56efd0d8d4adf201f6793a3ad990332b3a84909
Aranya Ghosh <83214223+Garanya@users.noreply.github.com>
2024-05-18 01:06:58 +0530

    Add function to play one round of the game.

--- script.js
+++ script.js
@@ -32,5 +32,27 @@ function getHumanChoice() {
     return user_choice;
 }
 
-console.log(getComputerChoice());
-console.log(getHumanChoice());
+function playRound(computer_choice, human_choice) {
+    
+    if((computer_choice === 'rock' && human_choice === 'scissors') || (computer_choice === 'paper' && human_choice === 'rock') || (computer_choice === 'scissors' && human_choice === 'paper')) {
+        console.log(`You lose! ${computer_choice} beats ${human_choice}`);
+        computer_score++;
+    }
+    
+    else if((computer_choice === 'rock' && human_choice === 'paper') || (computer_choice === 'paper' && human_choice === 'scissors') || (computer_choice === 'scissors' && human_choice === 'rock')) {
+        console.log(`You win! ${human_choice} beats ${computer_choice}`);
+        human_score++;
+    }
+    
+    else {
+        console.log(`It's a tie! ${computer_choice} matches ${human_choice}`);
+    }
+}
+
+/* console.log(getComputerChoice());
+console.log(getHumanChoice()); */
+let computer_score = 0;
+let human_score = 0;
+let computer_selection = getComputerChoice();
+let human_selection = getHumanChoice();
+playRound(computer_selection, human_selection);
