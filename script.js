5c031d4d8042b60d8201ef0e53a1f26719bdfe08
Aranya Ghosh <83214223+Garanya@users.noreply.github.com>
2024-05-18 02:00:56 +0530

    Add function to play 5 rounds of the game
    Function plays 5 rounds, keeps track of scores, and decides a winner
    at the end based on the scores.

--- script.js
+++ script.js
@@ -15,7 +15,7 @@ function getComputerChoice() {
     else {
         computer_choice = 'scissors';
     }
-
+    
     return computer_choice;
 }
 
@@ -34,25 +34,66 @@ function getHumanChoice() {
 
 function playRound(computer_choice, human_choice) {
     
+    let round_winner;
+
     if((computer_choice === 'rock' && human_choice === 'scissors') || (computer_choice === 'paper' && human_choice === 'rock') || (computer_choice === 'scissors' && human_choice === 'paper')) {
         console.log(`You lose! ${computer_choice} beats ${human_choice}`);
-        computer_score++;
+        round_winner = 'computer';
     }
     
     else if((computer_choice === 'rock' && human_choice === 'paper') || (computer_choice === 'paper' && human_choice === 'scissors') || (computer_choice === 'scissors' && human_choice === 'rock')) {
         console.log(`You win! ${human_choice} beats ${computer_choice}`);
-        human_score++;
+        round_winner = 'human';
     }
     
     else {
         console.log(`It's a tie! ${computer_choice} matches ${human_choice}`);
     }
+
+    return round_winner;
 }
 
-/* console.log(getComputerChoice());
-console.log(getHumanChoice()); */
-let computer_score = 0;
-let human_score = 0;
-let computer_selection = getComputerChoice();
-let human_selection = getHumanChoice();
-playRound(computer_selection, human_selection);
+
+function playGame() {
+    let computer_score = 0;
+    let human_score = 0;
+    let winner;
+    let final_winner;
+    let round = 1;
+
+    while(round <= 5){
+
+        let computer_selection = getComputerChoice();
+        let human_selection = getHumanChoice();
+
+        winner = playRound(computer_selection, human_selection);
+
+        if(winner === 'computer') {
+            computer_score++;
+        }
+        
+        if (winner === 'human') {
+            human_score++;
+        }
+
+        round++;
+    }
+
+    console.log(`Computer's score: ${computer_score}`);
+    console.log(`Your score: ${human_score}`);
+
+    if (computer_score > human_score) {
+        console.log(`Computer won the game by ${computer_score - human_score} points.`);
+    }
+
+    else if (computer_score < human_score) {
+        console.log(`You won the game by ${human_score - computer_score} points.`);
+    }
+
+    else {
+        console.log("It's a tie!");
+    }
+    
+}
+
+playGame();
